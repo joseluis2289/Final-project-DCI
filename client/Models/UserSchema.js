9c1ded2833b96bf2535a0210f80398878eae86b9
belccarvalho <belccarvalho@gmail.com>
2021-02-09 12:25:37 +0100

    create UserSchema

---
+++ client/Models/UserSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserSChema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  avatarPhoto: { type: String },
+  // gitHubAccount: {type: }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minLength: 8 },
+});
