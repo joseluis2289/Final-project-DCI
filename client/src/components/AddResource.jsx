24c5af522cfdb4c7c783b080d7fca0b3bb18cb5d
belccarvalho <belccarvalho@gmail.com>
2021-02-10 15:59:00 +0100

    add form handler

--- client/src/components/AddResource.jsx
+++ client/src/components/AddResource.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function AddResource() {
-  const [resource, setResource] = useState({});
   const [category, setCategory] = useState([]);
+  const [resource, setResource] = useState({});
 
   let defineCategory = (e) => {
     e.preventDefault();
@@ -15,11 +16,25 @@ export default function AddResource() {
     setResource({ ...resource, [e.target.id]: e.target.value });
   };
 
+  let addResource = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "references/add",
+      data: resource,
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={addResource}>
         <div>
-          <label for='title'>Title</label>
+          <label htmlFor='title'>Title</label>
           <input
             type='text'
             id='title'
@@ -28,7 +43,7 @@ export default function AddResource() {
           />
         </div>
         <div>
-          <label for='link'>Link</label>
+          <label htmlFor='link'>Link</label>
           <input
             type='text'
             id='link'
@@ -37,41 +52,41 @@ export default function AddResource() {
           />
         </div>
         <fieldset id='category' onChange={defineCategory}>
-          <label for='link'>Category</label>
-          <label for='frontend'>
+          <label htmlFor='link'>Category</label>
+          <label htmlFor='frontend'>
             <input type='checkbox' id='category' value='frontend' />
             Frontend
           </label>
 
-          <label for='backend'>
+          <label htmlFor='backend'>
             <input type='checkbox' id='category' value='backend' />
             Backend
           </label>
 
-          <label for='database'>
+          <label htmlFor='database'>
             <input type='checkbox' id='category' value='database' />
             Database
           </label>
 
-          <label for='general'>
+          <label htmlFor='general'>
             <input type='checkbox' id='category' value='general' />
             General
           </label>
         </fieldset>
         <fieldset onChange={formHandler}>
-          <label for='link'>Paid</label>
-          <label for='paid'>
+          <label htmlFor='link'>Paid</label>
+          <label htmlFor='paid'>
             <input type='checkbox' id='paid' value='paid' />
             Yes
           </label>
 
-          <label for='free'>
+          <label htmlFor='free'>
             <input type='checkbox' id='paid' value='free' />
             No
           </label>
         </fieldset>
         <div>
-          <label for='title'>description</label>
+          <label htmlFor='title'>description</label>
           <textarea
             id='description'
             name='description'
@@ -82,9 +97,7 @@ export default function AddResource() {
             onChange={formHandler}
           ></textarea>
         </div>
-        <button type='submit' class='btn btn-primary'>
-          Add Resource
-        </button>
+        <button type='submit'>Add Resource</button>
       </form>
     </div>
   );
