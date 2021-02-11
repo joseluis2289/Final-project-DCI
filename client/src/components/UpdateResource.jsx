22bc3c96288380618da128db32de33d68ddd1808
belccarvalho <belccarvalho@gmail.com>
2021-02-11 12:55:17 +0100

    add interpolation

--- client/src/components/UpdateResource.jsx
+++ client/src/components/UpdateResource.jsx
@@ -2,22 +2,39 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function UpdateResource() {
-  const [dataToUpdate, setDataToUpdate] = useState;
-  const [resource1, setResource1] = useState({ category: [] });
-  const [example, setExample] = useState({});
+  const [resource, setResource] = useState({
+    previewImage:
+      "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
+    userID: "usermodel",
+    category: ["database"],
+    edited: false,
+    deleted: false,
+    _id: "60251a5d1cd6cb4e195f81b7",
+    title: "Introduction to MongoDB",
+    link: "https://www.coursera.org/learn/introduction-mongodb?",
+    date: "2021-02-11T11:51:57.699Z",
+    rating: 4.3,
+    num_views: 41.946,
+    paid: "free",
+    description:
+      "This course will get you up and running with MongoDB quickly, and teach you how to leverage its power for data analytics. We'll start by mastering the fundamentals of MongoDB, including MongoDB’s Document data model, importing data into a cluster, working with our CRUD API and Aggregation Framework. These topics will be taught through a demo application which will give you a great first encounter of how simple and practical it can be to build applications with MongoDB.In addition to these essential topics, you will also learn and work with useful MongoDB tools and services. You will work with Atlas, MongoDB's database as a service, MongoDB Compass, a schema visualization tool, as well as many other useful command-line utilities.",
+    comments: [],
+    __v: 0,
+  });
 
   let formHandler = (e) => {
     e.preventDefault();
-    setExample({ ...example, [e.target.name]: e.target.value });
+    setResource({ ...resource, [e.target.name]: e.target.value });
   };
 
-  let updateResource1 = (e) => {
+  let updateResource = (e) => {
     e.preventDefault();
+    setResource({ ...resource, date: Date.now, edited: true });
     axios({
       method: "PUT",
-      url: `http://localhost:5000/resources/${_id}`,
+      url: `http://localhost:5000/resources/${resource.id}`,
       ContentType: "application-json",
-      data: example,
+      data: resource,
     })
       .then(function (response) {
         console.log(response);
@@ -31,7 +48,7 @@ export default function UpdateResource() {
     e.preventDefault();
     axios({
       method: "DEL",
-      url: `http://localhost:5000/resources/${_id}`,
+      url: `http://localhost:5000/resources/${resource.id}`,
     })
       .then(function (response) {
         console.log("resource1 deleted");
@@ -42,8 +59,8 @@ export default function UpdateResource() {
   };
   return (
     <div>
-      <form onSubmit={updateResource1}>
-        <button type={delResource}>X</button>
+      <form onSubmit={updateResource}>
+        <button onClick={delResource}>X</button>
         <div>
           <label htmlFor='title'>Title</label>
           <input
@@ -54,7 +71,7 @@ export default function UpdateResource() {
           />
         </div>
         <img
-          src={example.preventDefault}
+          src={resource.previewImg}
           alt='preview'
           style={{ width: "100px", height: "100px" }}
         ></img>
@@ -64,7 +81,7 @@ export default function UpdateResource() {
             type='text'
             name='link'
             placeholder='Enter the Link..'
-            value={example.link}
+            value={resource.link}
             onChange={formHandler}
           />
         </div>
@@ -112,7 +129,7 @@ export default function UpdateResource() {
             rows='5'
             cols='33'
             style={{ border: "solid black 2px" }}
-            value={example.description}
+            value={resource.description}
             placeholder='Enter your description...'
             onChange={formHandler}
           ></textarea>
