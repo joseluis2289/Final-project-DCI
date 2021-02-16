88128a18186da958fa8e2ecde08f779adb631c97
joseluis2289 <joseluis2289@gmail.com>
2021-02-16 13:08:00 +0100

    Modifying the UserModel

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   userName: { type: String, required: true },
   // avatarPhoto: { type: String },
   // gitHubAccount: {type: }
