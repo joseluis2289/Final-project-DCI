687b8ee8d5f48555ffc961a53563220c71655160
joseluis2289 <joseluis2289@gmail.com>
2021-02-16 14:42:36 +0100

    redefined UserModel

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  name: { type: String },
+  name: { type: String, required: true },
   userName: { type: String, required: true },
   // avatarPhoto: { type: String },
   // gitHubAccount: {type: }
