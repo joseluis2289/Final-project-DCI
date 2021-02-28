c8c12960005d6d255b974127a0dccd26ff6d2b29
belccarvalho <belccarvalho@gmail.com>
2021-02-28 21:09:42 +0100

    time ago on comments

--- client/src/components/settings/MyComments.jsx
+++ client/src/components/settings/MyComments.jsx
@@ -9,6 +9,32 @@ import Settings from "./Settings";
 export default function MyComments() {
   const user = useSelector((state) => state.user);
   const [userComments, setUserComments] = useState([]);
+  const timeSince = (date) => {
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  };
   useEffect(() => {
     axios
       .get(`http://localhost:5000/users/comments/${user._id}`)
@@ -31,6 +57,7 @@ export default function MyComments() {
               src={comment.resource.previewImage}
               alt='preview'
             ></img>
+            {timeSince(new Date(Date.now() - comment.date))}
             {comment.date}
             <p>count how much time ago</p>
           </div>
