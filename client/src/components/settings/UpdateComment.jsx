631ab72d5e3bd2a663166a35ad2291ef4d9ae8d9
belccarvalho <belccarvalho@gmail.com>
2021-03-01 13:06:52 +0100

    bedub updating

--- client/src/components/settings/UpdateComment.jsx
+++ client/src/components/settings/UpdateComment.jsx
@@ -3,34 +3,37 @@ import axios from "axios";
 
 export default function UpdateComment(props) {
   const [comment, setComment] = useState(props.data);
-
+  const [date, setDate] = useState(props.data.date);
   let formHandler = (e) => {
     setComment({
       ...comment,
-      edited: true,
-      date: Date.now,
       [e.target.name]: e.target.value,
     });
   };
 
   let updateComment = (e) => {
     e.preventDefault();
+    setComment({
+      ...comment,
+      edited: true,
+      date: Date.now,
+    });
     axios({
       method: "PUT",
-      url: `http://localhost:5000/comment/${comment._id}`,
+      url: `http://localhost:5000/comments/${comment._id}`,
       ContentType: "application/json",
       data: comment,
     })
       .then(function (response) {
-        console.log("responde from reqauest to update comment", response);
+        console.log("responde from request to update comment", response);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  let delComment = () => {
-    setComment({ ...comment, deleted: true, date: Date.now });
+  let delComment = (e) => {
+    e.preventDefault();
     axios({
       method: "PUT",
       url: `http://localhost:5000/comments/${comment._id}`,
@@ -47,16 +50,34 @@ export default function UpdateComment(props) {
 
   return (
     <div>
-      <form onSubmit={updateComment}>
-        <div className="delete-button">
-          <button onClick={delComment}>X</button>
-        </div>
-        {props.data.edited ? <span>Edited</span> : <span>Original</span>}
+      <div>
+        <button
+          className="delete-button"
+          onClick={(e) => {
+            setComment({ ...comment, deleted: true, date: Date.now });
+            delComment(e);
+          }}
+        >
+          X
+        </button>
+        {props.data.edited && (
+          <span>
+            Was already edited on{" "}
+            {date
+              .slice(0, 10)
+              .split("-")
+              .reverse()
+              .join("-")
+              .replaceAll("-", ".")}
+          </span>
+        )}
+      </div>
+      <form onSubmit={(e) => updateComment(e)}>
         <div>
           <label htmlFor="title">comment </label>
           {"  "}
           <textarea
-            name="description"
+            name="text"
             rows="10"
             cols="40"
             style={{ border: "solid black 2px" }}
