111acd88978a07719e6c5e2f5d5ba81ef994cfca
belccarvalho <belccarvalho@gmail.com>
2021-03-03 11:42:23 +0100

    display buttons

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateData } from "../redux/actions";
 import axios from "axios";
@@ -6,8 +6,45 @@ import ModalBox from "./ModalBox";
 
 export default function Comment(props) {
   const user = useSelector((state) => state.user);
+  const [comment, setComment] = useState(props.data);
+  const [edit, setEdit] = useState(false);
   const update = useSelector((state) => state.update);
   const dispatch = useDispatch();
+
+  //if user send request, the "edited" and "date" will be upated
+  useEffect(() => {
+    setComment({
+      ...comment,
+      edited: true,
+      date: Date.now(),
+    });
+  }, [edit]);
+
+  let displayButtons = (boolean) => {
+    setEdit(boolean);
+  };
+  let formHandler = (e) => {
+    setComment({
+      ...comment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  let updateComment = (e) => {
+    e.preventDefault();
+    axios({
+      method: "PUT",
+      url: `/comments/${comment._id}`,
+      ContentType: "application/json",
+      data: comment,
+    })
+      .then(function (response) {
+        response.data.nModified > 0 && dispatch(updateData(update));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   let delComment = () => {
     axios({
@@ -27,10 +64,6 @@ export default function Comment(props) {
   return (
     <div className="comment">
       <h3>{props.data.user.userName}</h3>
-      {props.data.edited && <span>Edited</span>}
-      {props.data.user._id === user._id && <ModalBox function={delComment} />}
-      <p>{props.data.text}</p>
-      {/* <input disabled>{props.data.text}</input> */}
       <span>
         {props.data.date
           .slice(0, 10)
@@ -39,6 +72,43 @@ export default function Comment(props) {
           .join("-")
           .replaceAll("-", ".")}
       </span>
+
+      <div>
+        {props.data.user._id === user._id && !edit && (
+          <div>
+            <ModalBox function={delComment} />{" "}
+            <button
+              onClick={() => {
+                displayButtons(true);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        )}
+      </div>
+      <form onSubmit={(e) => updateComment(e)}>
+        <input
+          disabled={
+            props.data.user._id === user._id && edit ? null : "disabled"
+          }
+          value={props.data.text}
+          name="text"
+        ></input>
+        {edit && (
+          <div>
+            <button
+              onClick={() => {
+                displayButtons(false);
+              }}
+            >
+              Cancel
+            </button>
+            <button>Save</button>
+          </div>
+        )}
+      </form>
+      {props.data.edited && <span>Edited</span>}
     </div>
   );
 }
