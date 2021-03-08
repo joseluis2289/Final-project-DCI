590b4d7aaef62ce2fcef0f532d29dfde50493bab
belccarvalho <belccarvalho@gmail.com>
2021-03-08 15:54:15 +0100

    resport resource

--- client/src/components/Resource/Options.jsx
+++ client/src/components/Resource/Options.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateData } from "../../redux/actions";
-import { Button, Dropdown, Header, Icon, Modal } from "semantic-ui-react";
+import {
+  Button,
+  Dropdown,
+  Header,
+  Icon,
+  Modal,
+  Form,
+  Radio,
+} from "semantic-ui-react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
@@ -9,19 +17,22 @@ import axios from "axios";
 export default function Options({ resource, userId }) {
   const user = useSelector((state) => state.user);
   const update = useSelector((state) => state.update);
-  const [share, setShare] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  const [reasonForReport, setReasonForReport] = useState("");
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [reportModal, setReportModal] = useState(false);
+
+  const handleChange = (e, { value }) => setReasonForReport(value);
 
   const dispatch = useDispatch();
   const options = [
     { key: "share", icon: "share", text: "Copy link", value: "share" },
-    { key: "attention", icon: "attention", text: "Report", value: "attention" },
+    { key: "report", icon: "attention", text: "Report", value: "report" },
   ];
   const optionsAuthor = [
     { key: "edit", icon: "edit", text: "Edit Post", value: "edit" },
     { key: "delete", icon: "delete", text: "Remove Post", value: "delete" },
     { key: "share", icon: "share", text: "Copy link", value: "share" },
-    { key: "attention", icon: "attention", text: "Report", value: "attention" },
+    { key: "report", icon: "attention", text: "Report", value: "report" },
   ];
   const notify = () => {
     toast.success("The link was copied!", {
@@ -32,12 +43,15 @@ export default function Options({ resource, userId }) {
 
   const handle = (e, { value }) => {
     if (value === "delete") {
-      setOpenModal(true);
+      setDeleteModal(true);
     }
     if (value === "share") {
       var url = `https://webdevelop-student-companion.herokuapp.com/resources/resource/${resource._id}`;
       navigator.clipboard.writeText(url);
       notify();
+    }
+    if (value === "report") {
+      setReportModal(true);
     }
   };
 
@@ -53,10 +67,12 @@ export default function Options({ resource, userId }) {
         />
       </Button.Group>
 
+      {/* MODAL TO DELETE RESOURCE */}
       <Modal
-        open={openModal}
-        onClose={() => setOpenModal(false)}
-        onOpen={() => setOpenModal(true)}
+        size="mini"
+        open={deleteModal}
+        onClose={() => setDeleteModal(false)}
+        onOpen={() => setDeleteModal(true)}
       >
         <Header icon="trash alternate" content="Delete Resource" />
         <Modal.Content>
@@ -66,7 +82,7 @@ export default function Options({ resource, userId }) {
           </p>
         </Modal.Content>
         <Modal.Actions>
-          <Button color="green" onClick={() => setOpenModal(false)}>
+          <Button color="green" onClick={() => setDeleteModal(false)}>
             <Icon name="remove" /> No
           </Button>
           <Button
@@ -79,7 +95,7 @@ export default function Options({ resource, userId }) {
               })
                 .then((response) => {
                   dispatch(updateData(update));
-                  setOpenModal(false);
+                  setDeleteModal(false);
                 })
                 .catch((err) => {
                   console.log(err);
@@ -87,6 +103,56 @@ export default function Options({ resource, userId }) {
             }}
           >
             <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
+
+      {/* MODAL TO REPORT */}
+      <Modal
+        size="mini"
+        open={reportModal}
+        onClose={() => setReportModal(false)}
+        onOpen={() => setReportModal(true)}
+      >
+        <Header icon="attention alternate" content="Report Content" />
+        <Modal.Content>
+          <Form>
+            <Form.Field>Why do you want to report this resource?</Form.Field>
+            <Form.Field>
+              <Radio
+                label="inappropriate content"
+                name="radioGroup"
+                value="inappropriate-content"
+                checked={reasonForReport === "inappropriate-content"}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label="repeated"
+                name="radioGroup"
+                value="repeated"
+                checked={reasonForReport === "repeated"}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label="other reason"
+                name="radioGroup"
+                value="other"
+                checked={reasonForReport === "other"}
+                onChange={handleChange}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="red" onClick={() => setReportModal(false)}>
+            <Icon name="remove" /> Cancel
+          </Button>
+          <Button color="green" onClick={() => setReportModal(false)}>
+            <Icon name="checkmark" /> Send
           </Button>
         </Modal.Actions>
       </Modal>
