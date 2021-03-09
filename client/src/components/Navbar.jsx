289a5573572cff70ea2573f2d65c213430af2529
belccarvalho <belccarvalho@gmail.com>
2021-03-09 11:36:47 +0100

    style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,134 +1,76 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { userLogout } from "../redux/actions";
-import { Grid, Menu, Header, Icon, Dropdown } from "semantic-ui-react";
+import { Dropdown, Icon } from "semantic-ui-react";
 import "./Navbar.css";
 
 export default function Navbar() {
   const logIn = useSelector((state) => state.logIn);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const history = useHistory();
+  const move = (e, { value }) => {
+    if (value === "/home") {
+      e.preventDefault();
+      dispatch(userLogout());
+    }
+    history.push(value);
+  };
+  const trigger = (
+    <span>
+      <Icon name="user" /> Hello, {`${user.name}`}
+    </span>
+  );
+
+  const loggedOptions = [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed in as <strong>{user.name}</strong>
+        </span>
+      ),
+      disabled: true,
+    },
+    { key: "profile", text: "Profile", value: "/profile" },
+    { key: "stars", text: "My Resources", value: "/my_resources" },
+    { key: "explore", text: "My Comments", value: "/my_comments" },
+    { key: "sign-out", text: "Sign Out", value: "/home" },
+  ];
+
   return (
-    <Grid columns={2} as="header" className="app-header">
-      <Grid.Row>
-        <Grid.Column textAlign={"center"} width={3} only="mobile">
-          <Dropdown item icon="bars" simple>
-            <Dropdown.Menu>
-              <Dropdown.Item>
-                <Link to="/home">
-                  <Icon name="home" />
-                  Home
-                </Link>
-              </Dropdown.Item>
-              {logIn ? (
-                <React.Fragment>
-                  <Dropdown.Item>
-                    <Link to="/settings">
-                      <Icon name="setting" />
-                      Settings
-                    </Link>
-                  </Dropdown.Item>
-
-                  <Dropdown.Item>
-                    <a
-                      href="/logout"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        console.log("LOGOUT!");
-                        dispatch(userLogout());
-                      }}
-                    >
-                      <Icon name="logout" />
-                      Logout
-                    </a>
-                  </Dropdown.Item>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  <Dropdown.Item>
-                    <Link to="/register">
-                      <Icon name="signup" />
-                      Register
-                    </Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <Link to="/login">
-                      <Icon name="sign-in" />
-                      Login
-                    </Link>
-                  </Dropdown.Item>
-                </React.Fragment>
-              )}
-            </Dropdown.Menu>
-          </Dropdown>
-        </Grid.Column>
-        <Grid.Column width={13} only="mobile">
-          <Link to="/">
-            <Header as="h1">Student Companion</Header>
+    <header className="app-header">
+      <Link to="/">(LOGO)</Link>
+      <h1>
+        <Link to="/">Student Companion</Link>
+      </h1>
+      {logIn ? (
+        <Dropdown trigger={trigger} options={loggedOptions} onChange={move} />
+      ) : (
+        <React.Fragment>
+          <Link to="/register">
+            <img
+              className="icon"
+              src="icons/icon_register.svg"
+              alt="Register Icon"
+            />
+            Register
           </Link>
-        </Grid.Column>
-
-        <Grid.Column width={9} only="tablet computer">
-          <Link to="/">
-            <Header as="h1">Student Companion</Header>
+          <Link to="/login">
+            <img className="icon" src="icons/icon_login.svg" alt="Login Icon" />
+            Login
           </Link>
-        </Grid.Column>
+        </React.Fragment>
+      )}
 
-        <Grid.Column
-          as="nav"
-          textAlign={"right"}
-          width={7}
-          only="tablet computer"
-        >
-          <Menu compact>
-            <Menu.Item>
-              <Link to="/home">
-                <Icon name="home" />
-                Home
-              </Link>
-            </Menu.Item>
-            {logIn ? (
-              <React.Fragment>
-                <Menu.Item>
-                  <Link to="/settings">
-                    <Icon name="setting" />
-                    Settings
-                  </Link>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <a
-                    href="/logout"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      console.log("LOGOUT!");
-                      dispatch(userLogout());
-                    }}
-                  >
-                    <Icon name="logout" />
-                    Logout
-                  </a>
-                </Menu.Item>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <Menu.Item>
-                  <Link to="/register">
-                    <Icon name="signup" />
-                    Register
-                  </Link>
-                </Menu.Item>
-                <Menu.Item>
-                  <Link to="/login">
-                    <Icon name="sign-in" />
-                    Login
-                  </Link>
-                </Menu.Item>
-              </React.Fragment>
-            )}
-          </Menu>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+      <nav>
+        {/* Icons from https://material.io/resources/icons/ */}
+        <Link to="/home">
+          <img className="icon" src="icons/home.svg" alt="Home Icon" />
+          Home
+        </Link>
+      </nav>
+    </header>
   );
 }
